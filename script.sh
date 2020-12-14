2819f03c7dc5d3277123d293e53b564e9a49e7c5
Ahmed Samy <64940965+Ahmedsamymahrous@users.noreply.github.com>
2020-12-14 16:11:15 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,45 @@
 #!/bin/bash
 
+cd ./DBMS
+Create_DB(){
+
+}
+
+Use_DB(){
+}
+
+Drop_DB(){
+}
+
+Show_DBs(){
+}
+
+Create_table(){
+}
+
+Drop_table(){
+}
+
+Show_tables(){ #Ensures uniqueness of PK
+
+}
+Check_uniquness(){
+
+}
+
+Insert(){
+}
+
+Delete_record(){
+}
+
+Update_record(){
+}
+
+Main(){
+select
+
+}
+
+Main
+
